664e9fb0827f438c92231dd0f51251d9c07e74d0
leidylmt <leidylmt.20@gmail.com>
2026-09-14 14:20:12 -0500

    Delete hola.cpp

--- hola.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main() {
-    int num;
-    cout << "ingrese numero: ";
-    cin >> num;
-    cout << "el numero ingresado es: " << num << endl;
-    return 0;
-}
