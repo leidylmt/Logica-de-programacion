8747581e2e10b5e9c326d70c038210fc67a74e61
leidylmt <leidylmt.20@gmail.com>
2026-09-14 13:56:41 -0500

    Add files via upload

---
+++ hola.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int num;
+    cout << "ingrese numero: ";
+    cin >> num;
+    cout << "el numero ingresado es: " << num << endl;
+    return 0;
+}
