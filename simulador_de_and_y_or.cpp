111e4e49bea72441d3373e5a754e5dae3ff1a852
leidylmt <leidylmt.20@gmail.com>
2026-09-14 14:35:33 -0500

    Create simulador_de_and_y_or.cpp

---
+++ simulador_de_and_y_or.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main() {
+    bool a, b;
+    cin >> a >> b;
+    cout << "AND: " << (a && b) << endl;
+    cout << "OR:  " << (a || b) << endl;
+    return 0;
+}
